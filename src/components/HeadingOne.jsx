e96d71c2d9c75d7e6d2942d957753a59938790d7
Mrleick <104718595+Mrleick@users.noreply.github.com>
2023-04-21 09:04:39 +0200

    stage one of app

---
+++ src/components/HeadingOne.jsx
@@ -0,0 +1,5 @@
+const HeadingOne = () => {
+  return <h1>MyMovies</h1>;
+};
+
+export default HeadingOne;
---
+++ src/components/Image.jsx
@@ -0,0 +1,7 @@
+import coverImage from "../assets/moviecover.png";
+
+const Image = () => {
+  return <img src={coverImage} alt="blablabla" />;
+};
+
+export default Image;
